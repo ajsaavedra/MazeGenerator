d11b934d530bb963a0fb451ec2ce5f6bd410ba48
Tony Saavedra <ajsaavedrax@gmail.com>
2016-06-01 22:15:50 -0700

    Setting up initial maze panel

---
+++ com/tonyjs/mazegenerator/Cell.java
@@ -0,0 +1,58 @@
+package com.tonyjs.mazegenerator;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Cell extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private static final int SIZE = 20;
+	private static final int TOP = 0;
+	private static final int RIGHT = 1;
+	private static final int BOTTOM = 2;
+	private static final int LEFT = 3;
+	
+	private int row = -1;
+	private int col = -1;
+	
+	private boolean[] wall = {true, true, true, true};
+	
+	public Cell(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+	
+	public boolean isWall(int index) {
+		return wall[index];
+	}
+	
+	public void paintComponent(Graphics g) {
+		// draw the background
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, SIZE, SIZE);
+		g.setColor(Color.BLACK);
+		
+		// draw the walls
+		if (isWall(TOP)) {
+			g.drawLine(0, 0, SIZE, 0);
+		}
+		if (isWall(LEFT)) {
+			g.drawLine(0, 0, 0, SIZE);
+		}
+	}
+	
+	public Dimension getPreferredSize() {
+		Dimension size = new Dimension(SIZE, SIZE);
+		return size;
+	}
+}
--- com/tonyjs/mazegenerator/MazeGenerator.java
+++ com/tonyjs/mazegenerator/MazeGenerator.java
@@ -1,14 +1,21 @@
 package com.tonyjs.mazegenerator;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 public class MazeGenerator extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private TitleLabel titleLabel = new TitleLabel("Maze");
+	private int rows = 5;
+	private int cols = 5;
+	private Cell[][] cell = new Cell[rows][cols];
+	private JPanel mazePanel = new JPanel();
 	
 	public MazeGenerator() {
 		initGUI();
@@ -22,6 +29,26 @@ public class MazeGenerator extends JFrame {
 	
 	public void initGUI() {
 		add(titleLabel, BorderLayout.PAGE_START);
+		
+		// center panel
+		JPanel centerPanel = new JPanel();
+		centerPanel.setBackground(Color.BLACK);
+		add(centerPanel, BorderLayout.CENTER);
+		
+		// maze panel
+		newMaze();
+		add(mazePanel, BorderLayout.CENTER);
+		
+	}
+	
+	public void newMaze() {
+		mazePanel.setLayout(new GridLayout(rows, cols));
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				cell[i][j] = new Cell(rows, cols);
+				mazePanel.add(cell[i][j]);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
